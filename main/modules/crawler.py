d2e1c0de4de17159a1047950f63e479a3e48a036
kensuke kubo <kensukekubo19@gmail.com>
2019-12-27 12:44:35 +0900

    ウィンドウサイズ

--- main/modules/crawler.py
+++ main/modules/crawler.py
@@ -12,6 +12,7 @@ class Crawler:
         options = Options()
         options.add_argument('--headless')
         self.driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', chrome_options=options)
+        self.driver.set_window_size(1920, 1080)
         self.driver.implicitly_wait(10)
         self.wait = Wait(self.driver)
         self.browser = Browser(self.driver)
