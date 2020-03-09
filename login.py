663e81d94e1d5344bbd13c6b2288ca2fbb020d60
kensuke kubo <kensukekubo19@gmail.com>
2020-03-09 15:12:50 +0900

    ログイン処理

--- login.py
+++ login.py
@@ -26,3 +26,17 @@ driver.find_element_by_xpath(
     '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
 time.sleep(10)
 ChatWork.send_screen(driver)
+
+driver.find_elements_by_css_selector('button')[1].click()
+time.sleep(2)
+ChatWork.send_screen(driver)
+
+driver.find_element_by_css_selector('input').send_keys(input('6桁：'))
+time.sleep(2)
+ChatWork.send_screen(driver)
+
+driver.find_element_by_xpath(
+    '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
+time.sleep(2)
+ChatWork.send_screen(driver)
+
