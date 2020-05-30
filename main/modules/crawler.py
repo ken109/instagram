d57d9d6162464c2732f5957c2776c4c3c496b6c3
kensuke kubo <kensukekubo19@gmail.com>
2020-05-30 18:08:31 +0900

    login chatwork

--- main/modules/crawler.py
+++ main/modules/crawler.py
@@ -7,6 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 
 from main.models import ScoreWord
+from main.modules.notify import ChatWork
 
 from .wait import Wait
 from .browser import Browser
@@ -41,12 +42,15 @@ class Crawler:
             '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input',
             wait_time=10).send_keys(
             'kubok.dev+instagram@gmail.com')
+        ChatWork.send_screen(self.driver)
         self.wait.find_element_by_xpath(
             '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input',
             wait_time=10).send_keys(
             'Kubo109Ken')
+        ChatWork.send_screen(self.driver)
         self.wait.find_element_by_xpath(
             '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button', wait_time=10).click()
+        ChatWork.send_screen(self.driver)
         time.sleep(10)
         # self.wait.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
 
