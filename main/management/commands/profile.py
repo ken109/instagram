fdf3cb552b0c3a450dd81ceb82cc3f824a95c9ca
kensuke kubo <kensukekubo19@gmail.com>
2020-05-30 18:23:22 +0900

    profileでログイン

--- main/management/commands/profile.py
+++ main/management/commands/profile.py
@@ -1,31 +1,33 @@
 from django.core.management.base import BaseCommand
 
 import chromedriver_binary
-from selenium import webdriver
 import time
 import re
 
 from main.models import Account
-from main.modules.crawler import get_chrome_options
+from main.modules.crawler import Crawler
+from main.modules.notify import ChatWork
 
 
 class Command(BaseCommand):
     help = 'update profile'
 
     def handle(self, *args, **options):
+        crawler = Crawler()
+        crawler.login()
         accounts = Account.objects.filter(invisible=0).order_by('-score').all()[:99]
-        driver = webdriver.Chrome(options=get_chrome_options())
 
         for account in accounts:
-            driver.get(account.url)
+            crawler.driver.get(account.url)
             try:
-                account.img = driver.find_element_by_xpath(
+                ChatWork.send_screen(crawler.driver)
+                account.img = crawler.driver.find_element_by_xpath(
                     '//*[@id="react-root"]/section/main/div/header/div/div/span/img').get_attribute('src')
-                follower = driver.find_element_by_xpath(
+                follower = crawler.driver.find_element_by_xpath(
                     '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span').get_attribute('title').replace(
                     ',', '')
                 account.follower = int(follower) if re.match('^[0-9]*$', follower) else 0
-                follow = driver.find_element_by_xpath(
+                follow = crawler.driver.find_element_by_xpath(
                     '//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span').text.replace(',', '')
                 account.follow = int(follow) if re.match('^[0-9]*$', follow) else 0
                 account.save()
@@ -33,5 +35,5 @@ class Command(BaseCommand):
                 pass
             time.sleep(40)
 
-        driver.close()
-        driver.quit()
+        crawler.driver.close()
+        crawler.driver.quit()
